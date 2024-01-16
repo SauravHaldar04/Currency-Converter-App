3270c0f2e6dc8abd0068f2c56f45ab58029f0069
Saurav Haldar <143389228+SauravHaldar04@users.noreply.github.com>
2024-01-16 14:46:31 +0530

    Adding new files

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\saura\\Downloads\\flutter_windows_3.13.2-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Web App Dev\\converter" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\saura\\Downloads\\flutter_windows_3.13.2-stable\\flutter"
+  "PROJECT_DIR=C:\\Web App Dev\\converter"
+  "FLUTTER_ROOT=C:\\Users\\saura\\Downloads\\flutter_windows_3.13.2-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Web App Dev\\converter\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Web App Dev\\converter"
+  "FLUTTER_TARGET=C:\\Web App Dev\\converter\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz1mYWxzZQ==,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9iMjAxODNlMDQwOTYwOTRiY2MzN2Q5Y2RlMmE0Yjk2ZjVjYzY4NGNmLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Web App Dev\\converter\\.dart_tool\\package_config.json"
+)
